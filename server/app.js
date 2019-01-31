e69fb662903590363f18a24cfd8f1745f2acecf8
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2019-01-31 16:01:35 +0800

    1.优化缓存,固化模板

--- server/app.js
+++ server/app.js
@@ -15,32 +15,38 @@ const app = new Koa()
 
 const router = new KoaRouter()
 
-router.get("/", ctx => {
-    const store = createNewStore()
-    const sheet = new ServerStyleSheet()
-    const html = ReactDOMServer.renderToString(sheet.collectStyles(
+//只需要构建一次
+const IndexBody = (function initPageBody() {
+    let store = createNewStore()
+    let sheet = new ServerStyleSheet()
+    let html = ReactDOMServer.renderToString(sheet.collectStyles(
         <Provider store={store}>
             <App></App>
         </Provider>
     ))
-    const styleTags = sheet.getStyleTags()
-    const body = ejs.render(
+    let styleTags = sheet.getStyleTags()
+    let body = ejs.render(
         template, 
         {
             style: styleTags,
             html
         }, 
         {delimiter: "?"})
-    ctx.body = body
+    return body
+}())
+
+router.get("/", ctx => {
+    ctx.body = IndexBody
 })
 
 router.get("/public/(.*)", (ctx, next) => {
     ctx.path = ctx.path.replace(/^\/public/, "") //去除前缀
     return next()
-}, require("koa-static")(path.resolve(__dirname, "../dist")))
+}, require("koa-static")(path.resolve(__dirname, "../dist"), {
+    maxage: 3600 * 24,
+    gzip: true
+}))
 
 app.use(router.routes()).use(router.allowedMethods())
-
-app.use(require("koa-static")(path.resolve(__dirname, "../dist")))
 
 app.listen(3000)
